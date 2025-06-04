25367c1fb5a84fc5b6cf9efec072f37c85beeebe
Nima <nima.m.memari@gmail.com>
2025-06-04 11:48:12 +0330

    Data_Mining -> jobs

--- app/Data_Mining.py
+++ app/Data_Mining.py
@@ -8,7 +8,7 @@ class Data_Mining():
     def __init__(self):
         self.driver = webdriver.Chrome()
 
-    def password(self, path):
+    def password(self, path:str):
         with open(path, "r") as file:
             lines = file.readlines()
             USERNAME = (lines[0].split(":", 1))[1].strip()
@@ -16,8 +16,8 @@ class Data_Mining():
         return USERNAME, PASSWORD
 
     def login(self):
-        Username, Password = Data_Mining.password(self, f"Linkedin-Job-Scraper/app/password.txt")
-        #opening site
+        Username, Password = self.password(f"Linkedin-Job-Scraper/app/password.txt")
+        #opening login url
         self.driver.get("https://www.linkedin.com/login/")
         self.driver.maximize_window()
         time.sleep(5)
@@ -33,5 +33,25 @@ class Data_Mining():
         password_input.send_keys(Keys.RETURN)
         time.sleep(5)
 
+    def jobs(self, job_name:str, location:str):
+        #opening jobs url
+        self.driver.get("https://www.linkedin.com/jobs/")
+        time.sleep(5)
+        #typing job name
+        job_name_input = self.driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Search by title, skill, or company"]')
+        job_name_input.clear()
+        job_name_input.send_keys(job_name)
+        time.sleep(1)
+        #typing location
+        location_input = self.driver.find_element(By.CSS_SELECTOR, 'input[aria-label="City, state, or zip code"]')
+        location_input.clear()
+        location_input.send_keys(location)
+        time.sleep(1)
+        #entering "Enter" key on keyboard
+        location_input.send_keys(Keys.RETURN)
+        time.sleep(5)
+
 data = Data_Mining()
 data.login()
+data.jobs('python', 'united states')
+
